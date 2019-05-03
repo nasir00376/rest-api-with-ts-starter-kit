545a0fce5bc97bc416c00dc73c4bd488ef6150d3
nasir00376 <nasir000376@gail.com>
2019-05-03 18:09:28 +0500

    Chok: Fix linting issues

--- config/db.ts
+++ config/db.ts
@@ -5,6 +5,6 @@ import { sequelize } from './sequelize';
 const debug: Debug.IDebugger = Debug('starter-kit:db');
 
 export async function connectDB(): Promise<void> {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
     debug('Database connected successfully');
 }
